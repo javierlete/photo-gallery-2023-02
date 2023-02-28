77b55d18245ac44aa77672ee402c8629dff31462
Javier Lete <jglete@eskura21.com>
2023-02-28 20:45:50 +0100

    Cargar fotos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Camera } from '@capacitor/camera';
 import { CameraResultType, CameraSource, Photo } from '@capacitor/camera/dist/esm/definitions';
 import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
-
   public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE: string = 'photos';
 
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
@@ -20,6 +21,25 @@ export class PhotoService {
     const savedImageFile = await this.savePicture(capturedPhoto);
     
     this.photos.unshift(savedImageFile);
+
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  }
+
+  public async loadSaved() {
+    const photoList = await Preferences.get({key: this.PHOTO_STORAGE});
+    this.photos = JSON.parse(photoList.value!) || [];
+
+    for(let photo of this.photos) {
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data
+      });
+
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
   }
 
   public async savePicture(photo: Photo) {
