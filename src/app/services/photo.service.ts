435f341f7969776d939684951764021cc660b55a
Javier Lete <jglete@eskura21.com>
2023-02-28 20:26:04 +0100

    Guardado de fichero en IndexedDB dentro de Apps

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Camera } from '@capacitor/camera';
-import { CameraResultType, CameraSource } from '@capacitor/camera/dist/esm/definitions';
+import { CameraResultType, CameraSource, Photo } from '@capacitor/camera/dist/esm/definitions';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +17,43 @@ export class PhotoService {
       quality: 100
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    })
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    
+    this.photos.unshift(savedImageFile);
   }
+
+  public async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    console.log('savedFile', savedFile);
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    }
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 export interface UserPhoto {
